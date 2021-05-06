7f48f6d1c4149c09a3fa6f83fe36553f267e904f
Thomas GRUEL <gruel@emse.fr>
2021-05-06 15:47:09 +0200

    add template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'angulartemp';
 }
